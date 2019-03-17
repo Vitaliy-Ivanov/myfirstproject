32b5334fa018f61da858d90d2f6a0d995870b957
Vitaliy Ivanov <48393077+Vitaliy-Ivanov@users.noreply.github.com>
2019-03-17 23:18:44 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
-ssh adming@192.168.0.102
-#!/bin/bash
-hostname
-sleep 5
-ifconfig
-sleep 5
-whoiam
-sleep 5
+pipeline {
+    agent { label 'master' }
+    stages {
+        stage('build') {
+            steps {
+                ssh adming@192.168.0.102
+                #!/bin/bash
+                echo "Hello World!"
+                sh "echo Hello from the shell"
+                sh "hostname"
+                sh "uptime"
+            }
+        }
+    }
+}
