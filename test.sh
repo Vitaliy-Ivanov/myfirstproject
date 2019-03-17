b0791b819e6db01885dc5c181961c666be646abe
Vitaliy Ivanov <48393077+Vitaliy-Ivanov@users.noreply.github.com>
2019-03-17 23:23:47 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,12 +3,8 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                #!/bin/bash
-                ssh adming@192.168.0.102
-                echo "Hello World!"
-                sh "echo Hello from the shell"
-                sh "hostname"
-                sh "uptime"
+                sh 'ssh adming@192.168.0.102'           
+                sh 'hostname'
             }
         }
     }
