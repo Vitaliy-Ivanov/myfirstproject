14e0c786f0fbaad90852ca1b01cd7cc22eed6314
Vitaliy Ivanov <48393077+Vitaliy-Ivanov@users.noreply.github.com>
2019-03-18 23:07:36 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,13 @@ pipeline {
                 sh '''
                 ssh adming@192.168.0.101       
                 hostname
+                sleep 5
+                ifconfig
+                uptime
+                sleep 5
+                whoiam
+                sleep 5
+                
                 '''
             }
         }
