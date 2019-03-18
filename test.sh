d31feeccf5c28604988ff77dc52554fc8285b358
Vitaliy Ivanov <48393077+Vitaliy-Ivanov@users.noreply.github.com>
2019-03-18 23:25:17 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,17 +3,13 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                sh '''
-                ssh adming@192.168.0.101       
-                hostname
-                sleep 5
                 
-                uptime
-                sleep 5
+                sh 'ssh adming@192.168.0.101'       
+                
                 sh 'whoiam'
-                sleep 5
                 
-                '''
+                
+                
             }
         }
     }
